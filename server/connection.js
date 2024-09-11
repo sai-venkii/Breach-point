ec08438df9735dd6c6984470181a610f89343117
sai-venkii <saivenketraj.03@gmail.com>
2024-09-11 11:05:46 +0530

    Mongo DB connection

--- server/connection.js
+++ server/connection.js
@@ -1,9 +1,14 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-mongoose.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.khcbk.mongodb.net/ctf?retryWrites=true&w=majority&appName=Cluster0`
-    )
-    .then(()=> {
-        console.log('connected to mongodb');
-    })
-    .catch(error => { console.error(error) });
+const Db_connect=async ()=>{
+    await mongoose.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.khcbk.mongodb.net/ctf?retryWrites=true&w=majority&appName=Cluster0`
+        )
+        .then(()=> {
+            console.log('Connected to Mongodb');
+        })
+        .catch(error => { console.error(error) });
+}
+
+module.exports = Db_connect;
+
--- server/server.js
+++ server/server.js
@@ -2,13 +2,8 @@ const express=require("express");
 const authRoute = require("./Auth/Auth-route");
 const mongoose = require("mongoose");
 const app=express();
+require('./connection')();
 
-const UserSchema=new mongoose.Schema({
-    name:String,
-    password:String
-})
-
-const userModel=mongoose.model('User',UserSchema)
 const PORT = process.env.PORT || 5500
 app.use('/',authRoute);
 app.listen(PORT,()=>{
