452454123a9188de717ec53bf585dd9753d2d055
sai-venkii <saivenketraj.03@gmail.com>
2024-09-27 22:49:32 +0530

    minor cors fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://breach-point.vercel.app",
+    origin: ["https://breach-point.vercel.app"],
     credentials:true
   })
 );
