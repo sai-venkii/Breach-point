f1a5da860668272da93755a33b0a86a3af39cf51
sai-venkii <saivenketraj.03@gmail.com>
2024-09-27 13:35:50 +0530

    base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // const API_BASE_URL = "http://localhost:8082";
-const API_BASE_URL = "https://breach-point-backend.onrender.com";
+const API_BASE_URL = "https://breach-point-backend.vercel.app";
 
 export default API_BASE_URL;
