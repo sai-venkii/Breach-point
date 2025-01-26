8b470858e1cdef34a52ccf4c00ee345e0271e5ab
AbhilaSh-30 <abhilashk2830@gmail.com>
2025-01-26 21:30:00 +0530

    Added default username and password

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import MatrixEffect from "./components/MatrixEffect";
 import axios from "axios";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 import API_BASE_URL from "./config";
 
 const validatePassword = (password) => ({
@@ -10,7 +10,7 @@ const validatePassword = (password) => ({
   uppercase: /[A-Z]/.test(password),
   lowercase: /[a-z]/.test(password),
   number: /\d/.test(password),
-  specialChar: /[@$!%*?&]/.test(password)
+  specialChar: /[@$!%*?&]/.test(password),
 });
 
 const Login = () => {
@@ -28,7 +28,7 @@ const Login = () => {
     uppercase: false,
     lowercase: false,
     number: false,
-    specialChar: false
+    specialChar: false,
   });
   const [showPasswordCriteria, setShowPasswordCriteria] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -44,7 +44,7 @@ const Login = () => {
     }
   }, [alertMessage]);
 
-  const toggleForm = () => setIsRegister(prevState => !prevState);
+  const toggleForm = () => setIsRegister((prevState) => !prevState);
 
   const handlePasswordChange = (event) => {
     const newPassword = event.target.value;
@@ -55,7 +55,11 @@ const Login = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if (!teamName || !password || (isRegister && (!email || !confirmPassword))) {
+    if (
+      !teamName ||
+      !password ||
+      (isRegister && (!email || !confirmPassword))
+    ) {
       setAlertType("error");
       setAlertMessage("Please fill in all required fields.");
       return;
@@ -66,16 +70,25 @@ const Login = () => {
       return;
     }
 
-    if (isRegister && (!passwordCriteria.length || !passwordCriteria.uppercase || !passwordCriteria.lowercase || !passwordCriteria.number || !passwordCriteria.specialChar)) {
+    if (
+      isRegister &&
+      (!passwordCriteria.length ||
+        !passwordCriteria.uppercase ||
+        !passwordCriteria.lowercase ||
+        !passwordCriteria.number ||
+        !passwordCriteria.specialChar)
+    ) {
       setPasswordError("Please meet all password criteria.");
       return;
     }
 
-    const url = isRegister ? `${API_BASE_URL}/auth/register` : `${API_BASE_URL}/auth/login`;
+    const url = isRegister
+      ? `${API_BASE_URL}/auth/register`
+      : `${API_BASE_URL}/auth/login`;
     const data = {
       teamName,
       password,
-      ...(isRegister && { email, confirmPassword })
+      ...(isRegister && { email, confirmPassword }),
     };
 
     try {
@@ -89,12 +102,13 @@ const Login = () => {
           }, 2000); // Wait for the alert message to show
         } else {
           setAlertMessage("Login successful! Redirecting to home...");
-          setTimeout(() => navigate('/home'), 2000);
+          setTimeout(() => navigate("/home"), 2000);
         }
       }
     } catch (error) {
-      console.error('Error:', error);
-      const message = error.response?.data?.message || "An error occurred. Please try again.";
+      console.error("Error:", error);
+      const message =
+        error.response?.data?.message || "An error occurred. Please try again.";
       setAlertType("error");
       setAlertMessage(message);
     }
@@ -102,32 +116,44 @@ const Login = () => {
 
   const headerVariants = {
     hidden: { opacity: 0, y: -20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeInOut" } },
-    exit: { opacity: 0, y: 20 }
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, ease: "easeInOut" },
+    },
+    exit: { opacity: 0, y: 20 },
   };
 
   const fieldVariants = {
     hidden: { opacity: 0, x: -20 },
     visible: { opacity: 1, x: 0, transition: { duration: 0.6, delay: 0.2 } },
-    exit: { opacity: 0, x: 20 }
+    exit: { opacity: 0, x: 20 },
   };
 
   const buttonVariants = {
     hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeInOut" } },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeInOut" },
+    },
     exit: { opacity: 0, y: 20 },
   };
 
   const buttonContainerVariants = {
     hidden: { opacity: 0, height: 0 },
     visible: { opacity: 1, height: "auto", transition: { duration: 0.6 } },
-    exit: { opacity: 0, height: 0 }
+    exit: { opacity: 0, height: 0 },
   };
 
   const confirmPasswordVariants = {
     hidden: { opacity: 0, height: 0 },
-    visible: { opacity: 1, height: "auto", transition: { duration: 0.8, delay: 0.3, ease: "easeInOut" } },
-    exit: { opacity: 0, height: 0, transition: { duration: 0.5 } }
+    visible: {
+      opacity: 1,
+      height: "auto",
+      transition: { duration: 0.8, delay: 0.3, ease: "easeInOut" },
+    },
+    exit: { opacity: 0, height: 0, transition: { duration: 0.5 } },
   };
 
   return (
@@ -135,7 +161,6 @@ const Login = () => {
       <MatrixEffect />
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="bg-black p-9 rounded-2xl text-white w-full max-w-md h-auto transition-all duration-700">
-
           {/* Alert Display */}
           <div className="absolute top-4 w-full max-w-md px-4">
             <AnimatePresence>
@@ -151,10 +176,18 @@ const Login = () => {
                   animate={{ opacity: 1 }}
                   exit={{ opacity: 0, transition: { duration: 1 } }}
                 >
-                  <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                  <svg
+                    className="flex-shrink-0 inline w-4 h-4 me-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
                   </svg>
-                  <span className="sr-only">{alertType === "success" ? "Success" : "Error"}</span>
+                  <span className="sr-only">
+                    {alertType === "success" ? "Success" : "Error"}
+                  </span>
                   <div>
                     <span className="font-medium"></span> {alertMessage}
                   </div>
@@ -227,7 +260,9 @@ const Login = () => {
             >
               <input
                 placeholder="Password"
-                className={`peer outline-none ring border-0 font-inter rounded-lg ring-gray-200 text-black duration-500 focus:ring-2 focus:border-gray-100 relative placeholder:duration-500 placeholder:absolute focus:placeholder:pt-10 shadow-xl shadow-gray-400/10 focus:shadow-none focus:rounded-md focus:ring-hacker-green placeholder:text-black ${passwordError ? 'border-red-500' : ''}`}
+                className={`peer outline-none ring border-0 font-inter rounded-lg ring-gray-200 text-black duration-500 focus:ring-2 focus:border-gray-100 relative placeholder:duration-500 placeholder:absolute focus:placeholder:pt-10 shadow-xl shadow-gray-400/10 focus:shadow-none focus:rounded-md focus:ring-hacker-green placeholder:text-black ${
+                  passwordError ? "border-red-500" : ""
+                }`}
                 type={passwordVisible ? "text" : "password"}
                 value={password}
                 onFocus={() => setShowPasswordCriteria(isRegister)}
@@ -240,19 +275,68 @@ const Login = () => {
               <button
                 type="button"
                 className="absolute right-4 top-1/2 transform -translate-y-1/2"
-                onClick={() => setPasswordVisible(prev => !prev)}
-              >
-              </button>
+                onClick={() => setPasswordVisible((prev) => !prev)}
+              ></button>
             </motion.div>
 
             {showPasswordCriteria && isRegister && (
-              <div className={`mt-2 text-sm ${!passwordCriteria.length || !passwordCriteria.uppercase || !passwordCriteria.lowercase || !passwordCriteria.number || !passwordCriteria.specialChar ? 'text-red-500' : 'text-gray-300'}`}>
+              <div
+                className={`mt-2 text-sm ${
+                  !passwordCriteria.length ||
+                  !passwordCriteria.uppercase ||
+                  !passwordCriteria.lowercase ||
+                  !passwordCriteria.number ||
+                  !passwordCriteria.specialChar
+                    ? "text-red-500"
+                    : "text-gray-300"
+                }`}
+              >
                 <ul>
-                  <li className={passwordCriteria.length ? 'text-green-500' : 'text-red-500'}>At least 8 characters long</li>
-                  <li className={passwordCriteria.uppercase ? 'text-green-500' : 'text-red-500'}>Includes at least one uppercase letter</li>
-                  <li className={passwordCriteria.lowercase ? 'text-green-500' : 'text-red-500'}>Includes at least one lowercase letter</li>
-                  <li className={passwordCriteria.number ? 'text-green-500' : 'text-red-500'}>Includes at least one number</li>
-                  <li className={passwordCriteria.specialChar ? 'text-green-500' : 'text-red-500'}>Includes at least one special character (!@#$)</li>
+                  <li
+                    className={
+                      passwordCriteria.length
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    At least 8 characters long
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.uppercase
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    Includes at least one uppercase letter
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.lowercase
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    Includes at least one lowercase letter
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.number
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    Includes at least one number
+                  </li>
+                  <li
+                    className={
+                      passwordCriteria.specialChar
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }
+                  >
+                    Includes at least one special character (!@#$)
+                  </li>
                 </ul>
               </div>
             )}
@@ -308,7 +392,9 @@ const Login = () => {
               exit="exit"
             >
               <span>
-                {isRegister ? "Already have an account?" : "Do not have an account?"}
+                {isRegister
+                  ? "Already have an account?"
+                  : "Do not have an account?"}
               </span>
               <motion.button
                 type="button"
@@ -319,6 +405,24 @@ const Login = () => {
               </motion.button>
             </motion.div>
           </form>
+          <div className="pt-4">
+            {!isRegister && (
+              <motion.div 
+                className="bg-white text-black p-4 rounded-lg shadow-md"
+                variants={fieldVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+              >
+                <p>
+                  <strong>Default Username:</strong> default
+                </p>
+                <p>
+                  <strong>Default Password:</strong> Default@1234
+                </p>
+              </motion.div>
+            )}
+          </div>
         </div>
       </div>
     </div>
